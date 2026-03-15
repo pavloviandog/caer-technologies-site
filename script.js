dce39bdcedd58336ee4b55a2076482c57bbf6ef7
pavloviandog <webmaster@consultcaer.com>
2026-03-15 11:48:56 -0500

    Initial CAER Technologies homepage
    Initial CAER Technologies homepage

---
+++ script.js
@@ -0,0 +1,15 @@
+// Simple scroll animation for buttons
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener('click', function (e) {
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+behavior: 'smooth'
+});
+
+});
+
+});
